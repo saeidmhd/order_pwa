5de83de740e85c08ee7ee17d12f213ec9f05a3df
Saeid Mohammadi <saeid.mhd@gmail.com>
2024-02-10 12:17:55 +0330

    adjust login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { LoginService } from '../login.service';
 
 @Component({
@@ -10,8 +10,33 @@ export class LoginComponent {
   formData: { username: string, password: string } = { username: '', password: '' };
   loginError: string = '';
   isLoading: boolean = false;
+  isKeyboardOpen = false;
 
   constructor(private loginService: LoginService) {}
+
+  @HostListener('window:resize')
+  onResize() {
+    this.adjustLayout();
+  }
+
+  adjustLayout() {
+    // Adjust your layout based on whether the keyboard is open
+    if (this.isKeyboardOpen) {
+      // The keyboard is open
+    } else {
+      // The keyboard is closed
+    }
+  }
+
+  onFocus() {
+    this.isKeyboardOpen = true;
+    this.adjustLayout();
+  }
+
+  onBlur() {
+    this.isKeyboardOpen = false;
+    this.adjustLayout();
+  }
 
   onSubmit() {
     const { username, password } = this.formData;
