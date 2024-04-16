eff1a531c329fa7167626b1c238ff193c3b99f7d
Mahsa <mhskhatami@gmail.com>
2024-04-16 11:35:44 +0330

    add visitor data to indexeddb

--- src/app/shared/services/authorizing/auth.service.ts
+++ src/app/shared/services/authorizing/auth.service.ts
@@ -4,10 +4,11 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 
 import { BazaraService } from '../bazara/bazara.service';
-import { IndexedDbService } from '../indexed-db/generic-indexed-db.service';
+import { GenericIndexedDbService } from '../indexed-db/generic-indexed-db.service';
 import { UtilityService } from '../common/utility.service';
 import { IBazaraLoginDTO } from '../../models/bazara-models/login-model/IBazaraLoginDTO';
 import { ILoginResult } from '../../models/bazara-models/login-model/ILoginResultDTO';
+import { IBazaraLogin } from '../../models/bazara-models/bazara-DTOs/IBazaraLogin';
 
 @Injectable({
   providedIn: 'root'
@@ -20,18 +21,20 @@ export class AuthService {
   constructor(private router: Router,
     private utilityService: UtilityService,
     private bazaraService: BazaraService,
-    private indexedDbService: IndexedDbService,
+    private GenericIndexedDbService: GenericIndexedDbService,
     private snackBar: MatSnackBar) { }
 
   loginToMobileOrdering(model: IBazaraLoginDTO): Observable<boolean> {    
     this.bazaraService.bazaraLogin(model).subscribe({
       next: (res: ILoginResult) => {
-        if (res.Result) {
-          //insert into indexeddb
-
-          this.indexedDbService.setVisitorId(res.Data!.VisitorId);
+        if (res.Result) {          
+          this.GenericIndexedDbService.setVisitorId(res.Data!.VisitorId.toString());
           localStorage.setItem('UserData', JSON.stringify(res.Data));
           localStorage.setItem('UserToken', res.Data!.UserToken);
+          
+          let loginData: IBazaraLogin[] = [];
+          loginData.push(res.Data!)
+          this.GenericIndexedDbService.insertingToDb('Login', loginData);
 
           this.utilityService.showMenuFooter.next(true);
 
@@ -53,7 +56,6 @@ export class AuthService {
       },
       error: (err) => {
         console.log(err);
-
       }
     });
     return this.isLoggedIn$;
@@ -68,9 +70,7 @@ export class AuthService {
 
   public isAuthenticated(): Promise<boolean> {
     return new Promise<boolean>((resolve, reject) => {
-      this.isLoggedIn$.subscribe(res => {
-        console.log(res, '*******isLoggedIn chie');
-        
+      this.isLoggedIn$.subscribe(res => {        
         resolve(res)
       });
       reject("get errors");
