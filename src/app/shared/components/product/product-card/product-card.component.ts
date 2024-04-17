f61a03635368c606a10495eed97677a46cd98f1a
Mahsa <mhskhatami@gmail.com>
2024-04-17 17:08:45 +0330

    clean code

--- src/app/shared/components/product/product-card/product-card.component.ts
+++ src/app/shared/components/product/product-card/product-card.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IBazaraProduct } from 'src/app/shared/models/bazara-models/bazara-DTOs/IBazaraProduct';
-import { IBazaraProductDetail } from 'src/app/shared/models/bazara-models/bazara-DTOs/IBazaraProductDetail';
 
 @Component({
   selector: 'app-product-card',
@@ -12,18 +11,6 @@ export class ProductCardComponent {
  
   @Input() product!: IBazaraProduct;
   isLoading = false;
-
-  getProductImageUrl(): string {
-
-    // const photoGallery = this.photoGalleries.find(pg => pg.ItemCode === product.ProductId);
-    // const picture = this.pictures.find(p => p.PictureId === photoGallery?.PictureId);
-    // if (picture) {
-    //   return 'https://mahakacc.mahaksoft.com' + picture.Url;
-    // } else {
-    //   return 'assets/img_empty_product.png'; // Path to the default image in your assets folder
-    // }
-    return 'assets/img_empty_product.png';
-  }
 
   decreaseQuantity() {
     
--- src/app/shared/components/product/product.component.ts
+++ src/app/shared/components/product/product.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable, ObservableInput, forkJoin, from } from 'rxjs';
-import { map, mergeMap, switchMap, tap } from 'rxjs/operators';
 
 import { GenericIndexedDbService } from '../../services/indexed-db/generic-indexed-db.service';
 import { IBazaraProduct } from '../../models/bazara-models/bazara-DTOs/IBazaraProduct';
@@ -20,9 +18,6 @@ import { IBazaraProductDetailStoreAsset } from '../../models/bazara-models/bazar
 export class ProductComponent implements OnInit {
 
   data: any[] = [];
-  // data: [IBazaraProduct[], IBazaraProductDetail[],
-  //        IBazaraVisitorProduct[], IBazaraProductDetailAssetStore[],
-  //        IBazaraPhotoGallery[], IBazaraPicture[] ] = [[],[],[],[],[],[]];
   products: IBazaraProduct[] = [];
 
   constructor(private genericIndexedService: GenericIndexedDbService) { }
@@ -33,16 +28,6 @@ export class ProductComponent implements OnInit {
   }
 
   async getData() {
-    // solution1: get data and make them observable
-    // await this.getProducts();
-    // await this.getProductDetail();
-    // await this.getVisitorProduct();
-    // return new Promise((resolve) => {
-    //   resolve();
-    // });
-
-    /// end of solution1
-
     // solution2: Promise all to get all data
     this.data.push(this.getProducts());
     this.data.push(this.getProductDetail());
@@ -57,10 +42,6 @@ export class ProductComponent implements OnInit {
         const visitorProduct = val[2].find((x: IBazaraVisitorProduct) => x.ProductDetailId == productDetail.ProductDetailId);
         const photoGallery = val[4].find((pg: IBazaraPhotoGallery) => pg.ItemCode === element.ProductId);
 
-        // console.log(photoGallery);
-        // console.log(picture);
-
-
         if (!visitorProduct.Deleted) {
           element.price = productDetail.Price1;
           element.count1 = val[3].find((x: IBazaraProductDetailStoreAsset) => x.ProductDetailId == productDetail.ProductDetailId).Count1;
@@ -74,27 +55,16 @@ export class ProductComponent implements OnInit {
           else {
             element.picUrl = 'assets/img_empty_product.png';
           }
-          console.log(element.picUrl);
 
           this.products.push(element);
-
-
         }
       });
     });
   }
 
-  // solution 2
-
-  setData() { }
-  // end of solution 2
-
-  //// solution 1
   async getProducts(): Promise<IBazaraProduct[]> {
     return new Promise((resolve, reject) => {
       this.genericIndexedService.getAllData<IBazaraProduct>('Product').then((products) => {
-        // console.log(products);
-        // this.productsSubject$.next(products) // solution1
         resolve(products);
       });
     });
@@ -103,8 +73,6 @@ export class ProductComponent implements OnInit {
   async getProductDetail(): Promise<IBazaraProductDetail[]> {
     return new Promise((resolve, reject) => {
       this.genericIndexedService.getAllData<IBazaraProductDetail>('ProductDetail').then(productDetails => {
-        // console.log(productDetails, 'Product details');
-        // this.productDetailsSubject.next(productDetails); // solution1
         resolve(productDetails);
       });
     });
@@ -113,9 +81,6 @@ export class ProductComponent implements OnInit {
   async getVisitorProduct(): Promise<IBazaraVisitorProduct[]> {
     return new Promise((resolve, reject) => {
       this.genericIndexedService.getAllData<IBazaraVisitorProduct>('VisitorProduct').then(visitorProducts => {
-        // console.log(visitorProducts, 'Product details');
-        // this.visitorProductSubject.next(visitorProducts); //solution 1
-
         resolve(visitorProducts);
       });
     });
@@ -144,65 +109,4 @@ export class ProductComponent implements OnInit {
       })
     })
   }
-  /// end of solution 1
 }
-
-
-
-
-// solution 1
-
-// private productsSubject$: BehaviorSubject<IBazaraProduct[]> = new BehaviorSubject<IBazaraProduct[]>([]);
-// get products$(): Observable<IBazaraProduct[]> {
-//   return this.productsSubject$.asObservable();
-// }
-
-// private productDetailsSubject: BehaviorSubject<IBazaraProductDetail[]> = new BehaviorSubject<IBazaraProductDetail[]>([]);
-// get productDetails$(): Observable<IBazaraProductDetail[]> {
-//   return this.productDetailsSubject.asObservable();
-// }
-
-// private visitorProductSubject: BehaviorSubject<IBazaraVisitorProduct[]> = new BehaviorSubject<IBazaraVisitorProduct[]>([]);
-// get visitorProducts$(): Observable<IBazaraVisitorProduct[]> {
-//   return this.visitorProductSubject.asObservable();
-// }
-
-
-// setProductName() {
-//   this.productDetails$.pipe(
-//     switchMap(pds => from(pds)),
-//     mergeMap((pd) =>
-//       this.products$.pipe(map((p) => {
-//         pd.ProductName = p.find(x => x.ProductId === pd.ProductId)?.Name!;
-//       }))
-//     )
-//   ).subscribe();
-// }
-
-// setProductCount() {
-
-//   // this.visitorProducts$.subscribe(res => console.log(res))
-//   this.productDetails$.pipe(
-//     switchMap(pds => from(pds)),
-//     mergeMap((pd) =>
-//       this.visitorProducts$.pipe(map((vp) => {
-//         console.log(vp, 'set product count');
-
-//         pd.Count1 = vp.find(x => x.ProductDetailId === pd.ProductDetailId && !x.Deleted)?.Count1!;
-//         pd.Count2 = vp.find(x => x.ProductDetailId === pd.ProductDetailId && !x.Deleted)?.Count2!;
-//       }))
-//     ),
-//   ).subscribe();
-// }
-
-
-// solution 3
-// setProductCount() {
-//   forkJoin({
-//     pd: this.productDetails$,
-//     p: this.products$,
-//     vp: this.visitorProducts$
-//   }).subscribe(res => {
-//     res.p.find
-//   })
-// }
