2594163dd4512a72cbfd24aabd262bd6af41b9e5
Mahsa Khatami <mhskhatami@gmail.com>
2024-04-24 10:38:17 +0330

    complete mark person on map

--- src/app/core/components/map/map.component.ts
+++ src/app/core/components/map/map.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from "leaflet";
 
+import { IBazaraPerson } from '../../models/bazara/bazara-DTOs/IBazaraPerson';
+import { GenericIndexedDbService } from '../../services/indexed-db/generic-indexed-db.service';
+import { IBazaraPersonAddress } from '../../models/bazara/bazara-DTOs/IBazaraPersonAddress';
+import { IPeople_Addresses, IPersonAddress } from '../../models/bazara/result-DTOs/IPeople_Addresses';
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -8,9 +13,29 @@ import * as L from "leaflet";
 })
 export class MapComponent implements OnInit {
   map!: L.Map;
+  icon = L.icon({
+    iconUrl: '/assets/icons/marker.svg'
+  });
 
+  iconOption = {
+    draggable: false, 
+    icon: this.icon
+  };
+
+  data: any[] = [];
+  people: IPeople_Addresses[] = [];
+  el: IPeople_Addresses = {
+    personId: 0,
+    personAddresses: [
+      // { address: '', latitude: 0, longitude: 0, mobile: 0, personAddressId: 0, title: '' }
+    ]
+  };
+
+  constructor(private genericIndexedDbService: GenericIndexedDbService) { }
+  
   ngOnInit(): void {
     this.initMap();
+    this.getData();
   }
 
   initMap() {
@@ -23,5 +48,61 @@ export class MapComponent implements OnInit {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(this.map);
+  }
+
+  async getData() {
+    this.data.push(this.getPeople());
+    this.data.push(this.getPersonAddresses());
+
+    Promise.all(this.data).then(val => {
+      this.compoundData(val);
+    });
+  }
+
+  async getPeople(): Promise<IBazaraPerson[]> {
+    return new Promise((resolve, reject) => {
+      this.genericIndexedDbService.getAllData<IBazaraPerson>('Person').then(people => {
+        resolve(people);
+      });
+    });
+  }
+
+  async getPersonAddresses(): Promise<IBazaraPersonAddress[]> {
+    return new Promise((resolve, reject) => {
+      this.genericIndexedDbService.getAllData<IBazaraPersonAddress>('PersonAddress').then(personAddresses => {
+        resolve(personAddresses);
+      });
+    });
+  }
+
+  compoundData(val: any) {
+    val[0].forEach((perosn: IBazaraPerson) => {
+      this.el = { personId: 0, personAddresses: [] }
+      this.el.personId = perosn.PersonId;
+      val[1].forEach((personAddress: IBazaraPersonAddress) => {
+        if (personAddress.PersonId == perosn.PersonId) {
+          this.el.personAddresses.push({
+            address: personAddress.Address,
+            latitude: personAddress.Latitude,
+            longitude: personAddress.Longitude,
+            mobile: personAddress.Mobile,
+            personAddressId: personAddress.PersonAddressId,
+            title: personAddress.Title
+          });
+        }
+      });
+      this.people.push(this.el);
+    });
+    this.setDataOnMap(this.people);
+  }
+
+  setDataOnMap(people: IPeople_Addresses[]) {
+    people.forEach(el => {
+      console.log(el);
+      el.personAddresses.forEach(element => {
+        let _marker = new L.Marker([element.latitude, element.longitude], this.iconOption).addTo(this.map);
+        
+      });
+    });
   }
 }
