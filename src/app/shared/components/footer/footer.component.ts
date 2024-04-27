fc82415a92b9a800874ffeaeeba0da32e4700c1b
Mahsa <mhskhatami@gmail.com>
2024-04-27 16:03:31 +0330

    dynamic footer for login- map and the others component

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UtilityService } from 'src/app/core/services/common/utility.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,4 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css'] // Use styleUrls instead of styleUrl
 })
 export class FooterComponent {
+  constructor(public utilityService: UtilityService) {}
 }
