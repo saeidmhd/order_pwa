0aa602ebabe4e921c769e699a49d03db2e41a3d3
Mahsa Khatami <mhskhatami@gmail.com>
2024-05-12 11:39:14 +0330

    add update-info ui

---
+++ src/app/core/models/bazara/get-all-data-DTOs/ReceviedBazaraData.ts
@@ -0,0 +1,8 @@
+export interface ReceivedBazaraData {
+    banksReceived: boolean,
+    visitorPeopleReceived: boolean,
+    peopleReceived: boolean,
+    productsReceived: boolean,
+    productDetailsReceived: boolean,
+
+}
