ef4c5dc472578d961fc7d7111080d23ecd29715f
Mahsa <mhskhatami@gmail.com>
2024-06-08 14:04:28 +0330

    delete order-detail from old model

--- src/app/shared/components/invoice/invoice.component.ts
+++ src/app/shared/components/invoice/invoice.component.ts
@@ -3,7 +3,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { PropertyDescription } from 'src/app/core/models/bazara/bazara-DTOs/property-description';
 import { Person } from 'src/app/core/models/old/Person';
 import { Order } from 'src/app/core/models/old/order';
-import { OrderDetail } from 'src/app/core/models/old/order-detail';
+import { OrderDetail } from 'src/app/core/models/bazara/bazara-DTOs/order-detail';
 import { Product } from 'src/app/core/models/old/product';
 import { ProductDetail } from 'src/app/core/models/old/product-detail';
 import { IndexedDbService } from 'src/app/core/services/indexed-db/indexed-db.service';
