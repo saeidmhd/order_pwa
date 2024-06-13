ec46873195b1e9d91a2dfaf496388558c716d7f0
Mahsa <mhskhatami@gmail.com>
2024-06-13 10:16:02 +0330

    clean matrial modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,58 +1,34 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-
 import { HttpClientModule } from '@angular/common/http';
-
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-
-
-import { LoginComponent } from './shared/components/login/login.component'; // Import the component
-import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { PeopleListComponent } from './shared/components/people-list/people-list.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './shared/modules/material/material.module';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './shared/components/login/login.component'; // Import the component
+import { PeopleListComponent } from './shared/components/people-list/people-list.component';
 import { DashboardComponent } from './shared/components/dashboard/dashboard.component';
 import { BasicInfoComponent } from './shared/components/basic-info/basic-info.component';
 import { BankListComponent } from './shared/components/bank-list/bank-list.component';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { OrderListComponent } from './shared/components/order-list/order-list.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { ProductCategoriesComponent } from './shared/components/product-categories/product-categories.component';
 import { OrderDetailComponent } from './shared/components/order-detail/order-detail.component';
 import { ToolbarComponent } from './shared/components/toolbar/toolbar.component';
 import { LogUpdateService } from './core/services/pwa_services/log-update.service';
 import { PromptUpdateService } from './core/services/pwa_services/prompt-update.service';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatSelectModule } from '@angular/material/select';
-import { MatTableModule } from '@angular/material/table';
-import {MatChipsModule} from '@angular/material/chips';
 
-
-import {
-  MatDialogActions,
-  MatDialogClose,
-  MatDialogTitle,
-  MatDialogContent,
-} from '@angular/material/dialog';
 import { UpdateConfirmationDialogComponent } from './shared/components/update-confirmation-dialog/update-confirmation-dialog.component';
 import { InvoiceComponent } from './shared/components/invoice/invoice.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { DrawerComponent } from './shared/components/drawer/drawer.component';
 import { MapComponent } from './shared/components/map/map.component';
-import { MaterialModule } from './shared/modules/material/material.module';
-import { CommonModule } from '@angular/common';
 import { ProductComponent } from './shared/components/product/product.component';
 import { ProductCardComponent } from './shared/components/product/product-card/product-card.component';
 import { MiniReportComponent } from './shared/components/dashboard/mini-report/mini-report.component';
@@ -105,24 +81,7 @@ import { OrderCardComponent } from './shared/components/order/order-card/order-c
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    MatProgressSpinnerModule,
-    MatSidenavModule,
     BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatButtonModule,
-    MatIconModule,
-    MatInputModule,
-    MatListModule,
-    MatCardModule,
-    MatProgressBarModule,
-    MatToolbarModule,
-    MatDialogActions,
-    MatDialogClose,
-    MatDialogTitle,
-    MatDialogContent,
-    MatSelectModule,
-    MatTableModule,
-    MatChipsModule,
     MaterialModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
--- src/app/shared/modules/material/material.module.ts
+++ src/app/shared/modules/material/material.module.ts
@@ -1,3 +1,4 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -7,6 +8,19 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent } from '@angular/material/dialog';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const MaterialComponents = [
   MatSnackBarModule,
@@ -14,7 +28,23 @@ const MaterialComponents = [
   MatExpansionModule,
   MatDividerModule,
   MatBadgeModule,
-  MatMenuModule
+  MatMenuModule,
+  MatFormFieldModule,
+  MatButtonModule,
+  MatInputModule,
+  MatIconModule,
+  MatProgressSpinnerModule,
+  MatSidenavModule,
+  MatListModule,
+  MatSelectModule,
+  MatTableModule,
+  MatChipsModule,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogTitle,
+  MatDialogContent,
+  MatProgressBarModule,
+  MatToolbarModule
 ];
 
 @NgModule({
